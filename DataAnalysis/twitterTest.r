227864948d932ef4ba7d86aedab47b9c12a481b2
zpeng-projects <jerry.peng2013@hotmail.com>
2014-07-02 19:01:11 -0400

    1.22

--- DataAnalysis/twitterTest.r
+++ DataAnalysis/twitterTest.r
@@ -3,6 +3,7 @@ rm(list=ls())
 
 library(devtools)
 library(twitteR)
+library(ggplot2)
 
 api_key <- "HJB9l39OhH7XKqr6deYHROft6"
 
@@ -46,4 +47,56 @@ sources<-sapply(sources,function(x)ifelse(length(x)>1,x[2],x[1]))
 source_table=table(sources)
 pie(source_table[source_table>10])
 
-For this we need a list of words that contains positive and negative sentiment words. I have downloaded the list from Google and it is easily available.
+#For this we need a list of words that contains positive and 
+#negative sentiment words. I have downloaded the list from Google 
+#and it is easily available.
+txt<- VectorSource(text.d)
+txt.corpus<-Corpus(txt)
+inspect(txt.corpus)
+txt.corpus<-tm_map(txt.corpus,tolower)
+txt.corpus<-tm_map(txt.corpus,removePunctuation)
+txt.corpus<-tm_map(txt.corpus,removeNumbers)
+txt.corpus<-tm_map(txt.corpus,removeWords,stopwords("english"))
+
+#Next do stemming: load the ‘SnowballC’ package (Bouchet-Valat, 2013) which
+#allows us to identify specific stem elements using the ‘tm
+#map’ function of the ‘tm’ package
+library(snowballC)
+txt.corpus<-tm_map(txt.corpus,stemDocument)
+detach("package:snowballC")
+inspect(txt.corpus)
+
+txt.corpus<-tm_map(txt.corpus,stripWhitespace)
+inspect(txt.corpus)
+
+#create something called a Term Document Matrix (TDM),
+#hich is a matrix of frequency counts for each word used in the corpus
+tdm<-TermDocumentMatrix(txt.corpus)
+inspect(tdm[1:20,])
+
+#Next, we can begin to explore the TDM, using the ‘findFreqTerm
+#s’ function, to find which words were used most.
+findFreqTerms(x=tdm,lowfreq=8,highfreq=Inf)
+
+#Next, we can use the ‘findAssocs’ function to find words which 
+#associate together. Here, we are specifying the TDM to use,
+#the term we want to find associates for, and the lowest 
+#acceptable correlation limit
+# This returns a vector of terms which are associated with 
+#‘comput’ at = 0.60 or more (correlation) – and reports each 
+#association in descending order.
+findAssocs(x=tdm,term="comput",corlimit=0.6)
+
+#If desired, terms which occur very infrequently (i.e. sparse terms)
+#can be removed; leaving only the ‘common’ terms. Below, the
+#‘sparse’ argument refers to the MAXIMUM sparse-ness allowed for
+#a term to be in the returned matrix; in other words, the larger 
+#the percentage, the more terms will be retained(the smaller the 
+#percentage, the fewer [but more common] terms will be retained
+tdm.com.60<-removeSparseTerms(x=tdm,sparse=0.6)
+
+##-------------------------------
+library(twitteR)
+delta.tweets<-searchTwitter("@delta",n=1500)
+
+
