aeb58eecfe5187707b8efb3119179d7600afea4c
zpeng-projects <jerry.peng2013@hotmail.com>
2014-07-10 01:33:14 -0400

    test html ui

--- DataAnalysis/server.R
+++ DataAnalysis/server.R
@@ -4,8 +4,7 @@ library(quantmod)
 library(TTR)
 library(MASS)
 library(class)
-library(tm)
-library(SnowballC) 
+
 api_key <- "HJB9l39OhH7XKqr6deYHROft6"
 api_secret <- "l4ZyUExemzqPnEeD5qtw5aHuuN8oSMCOe7pNSNaE7lTVHzuGYI"
 access_token <- "2534082073-qlC6nlEgvmHb1zzJCqDhZhtIqIAYgZZBDJcOVra"
@@ -31,6 +30,8 @@ shinyServer(function(input, output) {
   })
   
   prep_data <- reactive({ 
+    library(tm)
+    library(SnowballC) 
     tw_df<-data()
     raw_tweet<-tw_df$text
     tw_df$text<-sapply(tw_df$text,func_removeNonAscii)
