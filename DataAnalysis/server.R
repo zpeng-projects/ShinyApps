e80a747191f8a8cd4282c77019f246bcf60c7ed6
zpeng-projects <jerry.peng2013@hotmail.com>
2014-07-10 00:01:17 -0400

    test html ui

--- DataAnalysis/server.R
+++ DataAnalysis/server.R
@@ -10,43 +10,19 @@ api_key <- "HJB9l39OhH7XKqr6deYHROft6"
 api_secret <- "l4ZyUExemzqPnEeD5qtw5aHuuN8oSMCOe7pNSNaE7lTVHzuGYI"
 access_token <- "2534082073-qlC6nlEgvmHb1zzJCqDhZhtIqIAYgZZBDJcOVra"
 access_token_secret <- "GSdNj3Clasqm4Ub5Nl647y8cuafphvPi5NpcHkdasFk0p"
-#setup_twitter_oauth(api_key,api_secret,access_token,access_token_secret)
+setup_twitter_oauth(api_key,api_secret,access_token,access_token_secret)
+
+func_removeNonAscii<-function(x) {
+  dat2 <- unlist(strsplit(x, split=" "))
+  dat3 <- grepl("dat2", iconv(dat2, "latin1", "ASCII", sub="dat2"))
+  dat4 <- dat2[!dat3]
+  dat5 <- paste(dat4, collapse = " ")
+}
+
+hu.liu.pos<-scan("positive-words.txt", what="character", comment.char=";")
+hu.liu.neg<-scan("negative-words.txt", what="character", comment.char=";")
 
 shinyServer(function(input, output) {
   
-  # Reactive expression to generate the requested distribution. This is 
-  # called whenever the inputs change. The output renderers defined 
-  # below then all used the value computed from this expression
-  data <- reactive({  
-    dist <- switch(input$dist,
-                   norm = rnorm,
-                   unif = runif,
-                   lnorm = rlnorm,
-                   exp = rexp,
-                   rnorm)
-    
-    dist(input$n)
-  })
   
-  # Generate a plot of the data. Also uses the inputs to build the 
-  # plot label. Note that the dependencies on both the inputs and
-  # the data reactive expression are both tracked, and all expressions 
-  # are called in the sequence implied by the dependency graph
-  output$plot <- renderPlot({
-    dist <- input$dist
-    n <- input$n
-    
-    hist(data(), 
-         main=paste('r', dist, '(', n, ')', sep=''))
-  })
-  
-  # Generate a summary of the data
-  output$summary <- renderPrint({
-    summary(data())
-  })
-  
-  # Generate an HTML table view of the data
-  output$table <- renderTable({
-    data.frame(x=data())
-  })
 })
