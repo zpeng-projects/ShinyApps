80d577cbc1981fdd7c7af8c27ee3097fe0d416a4
zpeng-projects <jerry.peng2013@hotmail.com>
2014-07-10 00:03:41 -0400

    test html ui

--- DataAnalysis/server.R
+++ DataAnalysis/server.R
@@ -10,7 +10,6 @@ api_key <- "HJB9l39OhH7XKqr6deYHROft6"
 api_secret <- "l4ZyUExemzqPnEeD5qtw5aHuuN8oSMCOe7pNSNaE7lTVHzuGYI"
 access_token <- "2534082073-qlC6nlEgvmHb1zzJCqDhZhtIqIAYgZZBDJcOVra"
 access_token_secret <- "GSdNj3Clasqm4Ub5Nl647y8cuafphvPi5NpcHkdasFk0p"
-setup_twitter_oauth(api_key,api_secret,access_token,access_token_secret)
 
 func_removeNonAscii<-function(x) {
   dat2 <- unlist(strsplit(x, split=" "))
@@ -23,6 +22,13 @@ hu.liu.pos<-scan("positive-words.txt", what="character", comment.char=";")
 hu.liu.neg<-scan("negative-words.txt", what="character", comment.char=";")
 
 shinyServer(function(input, output) {
+  
+  data <- reactive({  
+    setup_twitter_oauth(api_key,api_secret,access_token,access_token_secret)
+    tweets<-searchTwitter({input$tw_query}, n={input$tw_number}*1.2,lang="en")
+    tw_df<-do.call("rbind", lapply(tweets, as.data.frame))     
+  })
+  
   
   
 })
--- DataAnalysis/server_real.R
+++ DataAnalysis/server_real.R
@@ -6,21 +6,6 @@ library(quantmod)
 library(TTR)
 library(MASS)
 library(class)
-api_key <- "HJB9l39OhH7XKqr6deYHROft6"
-api_secret <- "l4ZyUExemzqPnEeD5qtw5aHuuN8oSMCOe7pNSNaE7lTVHzuGYI"
-access_token <- "2534082073-qlC6nlEgvmHb1zzJCqDhZhtIqIAYgZZBDJcOVra"
-access_token_secret <- "GSdNj3Clasqm4Ub5Nl647y8cuafphvPi5NpcHkdasFk0p"
-setup_twitter_oauth(api_key,api_secret,access_token,access_token_secret)
-
-func_removeNonAscii<-function(x) {
-  dat2 <- unlist(strsplit(x, split=" "))
-  dat3 <- grepl("dat2", iconv(dat2, "latin1", "ASCII", sub="dat2"))
-  dat4 <- dat2[!dat3]
-  dat5 <- paste(dat4, collapse = " ")
-}
-
-hu.liu.pos<-scan("positive-words.txt", what="character", comment.char=";")
-hu.liu.neg<-scan("negative-words.txt", what="character", comment.char=";")
 
 shinyServer(function(input, output) {
     
@@ -82,10 +67,7 @@ shinyServer(function(input, output) {
         df<-df[m:j,]    
       })
   
-  data <- reactive({      
-    tweets<-searchTwitter({input$tw_query}, n={input$tw_number}*1.2,lang="en")
-    tw_df<-do.call("rbind", lapply(tweets, as.data.frame))     
-  })
+  
   
   prep_data <- reactive({ 
     tw_df<-data()
